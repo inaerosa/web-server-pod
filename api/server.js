fb039164d33d05e3c62e96871b7586debc171db6
Inaê Rosa <inaedb.rosa@gmail.com>
2022-06-03 13:07:56 -0300

    set view engine, views and methodOverride

--- api/server.js
+++ api/server.js
@@ -7,7 +7,22 @@ const routes = require("./routes/userRoutes")
 dotenv.config()
 app.use(express.json())
 
+const path = require('path')
+const methodOverride = require('method-override')
+
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use("/clients", routes)
+
+app.get("/", (req, res) => {
+  res.render('Home')
+})
 
 app.listen(3000, ()=>{
   console.log('Server running at 3000')
