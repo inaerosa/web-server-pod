dc507b4144fe058b0b0fb91d8f1d3125bf514897
Inaê Rosa <inaedb.rosa@gmail.com>
2022-06-20 11:05:44 -0300

    refactor: structure project

--- api/server.js
+++ api/server.js
@@ -7,17 +7,6 @@ const routes = require("./routes/userRoutes");
 dotenv.config();
 app.use(express.json());
 
-const path = require("path");
-const methodOverride = require("method-override");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
 app.use("/clients", routes);
 
 app.listen(3000, () => {
